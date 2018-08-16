284700f0b5b61592ca7cc7fed2164a74a87ed375
James Kageni <james.kageni@gmail.com>
2018-08-16 16:51:56 +0300

    added the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { DevComponent } from './dev/dev.component';
+import { RepoComponent } from './repo/repo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFormComponent,
+    DevComponent,
+    RepoComponent
   ],
   imports: [
     BrowserModule
